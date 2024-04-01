14be5493bb34c82a19fad20c33c6da6b47e41d2e
Pavel Buryakovsky <buryakovsky.pavel@gmail.com>
2024-04-02 02:45:28 +0500

    first commit

---
+++ homework_callbook.py
@@ -0,0 +1,80 @@
+'''
+Семинар 8. Работа с файлами
+Задача №49. Решение в группах
+Создать телефонный справочник с
+возможностью импорта и экспорта данных в
+формате .txt. Фамилия, имя, отчество, номер
+телефона - данные, которые должны находиться
+в файле.
+1. Программа должна выводить данные
+2. Программа должна сохранять данные в
+текстовом файле
+3. Пользователь может ввести одну из
+характеристик для поиска определенной
+записи(Например имя или фамилию
+человека)
+4. Использование функций. Ваша программа
+не должна быть линейной
+'''
+
+from csv import DictReader, DictWriter
+from os.path import exists
+
+file_name = 'phone.csv'
+
+
+def get_info():
+    first_name = 'Ivan'
+    last_name = 'Ivanov'
+    flag = False
+    while not flag:
+        try:
+            phone_number = int(input('Введите телефон: '))
+            if len(str(phone_number)) != 11:
+                print('Некорректная длина номера')
+            else:
+                flag = True
+        except ValueError:
+            print('Невалидный номер')
+    return [first_name, last_name, phone_number]
+
+
+def create_file(file_name):
+    with open(file_name, 'w', encoding='utf-8', newline='') as data:
+        f_w = DictWriter(data, fieldnames=['имя', 'фамилия', 'телефон'])
+        f_w.writeheader()
+
+
+def read_file(file_name):
+    with open(file_name, 'r', encoding='utf-8') as data:
+        f_r = DictReader(data)
+        return list(f_r)
+
+
+def write_file(file_name, lst):
+    res = read_file(file_name)
+    obj = {'имя': lst[0], 'фамилия': lst[1], 'телефон': lst[2]}
+    res.append(obj)
+    with open(file_name, 'w', encoding='utf-8', newline='') as data:
+        f_w = DictWriter(data, fieldnames=['имя', 'фамилия', 'телефон'])
+        f_w.writeheader()
+        f_w.writerows(res)
+
+
+def main():
+    while True:
+        command = input('Введите команду: ')
+        if command == 'q':
+            break
+        elif command == 'w':
+            if not exists(file_name):
+                create_file(file_name)
+            write_file(file_name, get_info())
+        elif command == 'r':
+            if not exists(file_name):
+                print('Файл отсутствует, создайте его')
+                continue
+            print(*read_file(file_name), sep='\n')
+
+
+main()
